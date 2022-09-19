6b53535d1754b901fdb10b1c8e27312af08599b0
Kevin Castillo <kevincastillo318@gmail.com>
2022-09-19 07:46:19 -0700

    pages & routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/Signup.js
@@ -0,0 +1,9 @@
+import "./Signup.module.css";
+
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+    </div>
+  );
+}
