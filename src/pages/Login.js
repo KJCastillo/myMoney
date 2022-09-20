b5b2889a7ee081997f4b7b2f48bcc4e0c5a37b6d
Kevin Castillo <kevincastillo318@gmail.com>
2022-09-20 06:46:28 -0700

    login form made

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,38 @@
 import styles from "./Login.module.css";
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //intial value of empty string when using state with input fields
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    // class name has a hyphen, reason for diff format on form tag
+    // global css index.css, reason for diff format on button tag
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
   );
 }
