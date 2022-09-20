2236225e009daca75a279a866e9ee4b385257416
Kevin Castillo <kevincastillo318@gmail.com>
2022-09-20 07:17:49 -0700

    imported firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBhfJwoFw17RJVI4XhadROCYDee-CtYkKQ",
+    authDomain: "mymoney-f894c.firebaseapp.com",
+    projectId: "mymoney-f894c",
+    storageBucket: "mymoney-f894c.appspot.com",
+    messagingSenderId: "392664010801",
+    appId: "1:392664010801:web:5109d07c5b8e04373ac510"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init service
+  const projectFirestore = firebase.firestore()
+
+  export { projectFirestore }
