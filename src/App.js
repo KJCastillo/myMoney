9580bac2f7227ff52690f0c92b0c146272de7435
Kevin Castillo <kevincastillo318@gmail.com>
2022-09-20 06:33:45 -0700

    navbar created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
