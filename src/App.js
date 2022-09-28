0dda4889368172ae22fefd644b6768ff95df2e18
Kevin Castillo <kevincastillo318@gmail.com>
2022-09-28 08:03:31 -0700

    route guarding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const { authIsReady } = useAuthContext();
+  const { authIsReady, user } = useAuthContext();
 
   return (
     <div className="App">
@@ -15,17 +15,26 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path="/signup">
-              <Signup />
+              {!user && <Redirect to="/login" />}
+              {user && <Home />}
+              {/* if user is null, only shows login page */}
+              {/* if there is a user, access to home page */}
             </Route>
           </Switch>
           <Switch>
             <Route path="/login">
-              <Login />
+              {user && <Redirect to="/" />}
+              {!user && <Login />}
+              {/* if there is a user, redirects to home after login */}
+              {/* if user is null, only shows login page */}
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/signup">
+              {user && <Redirect to="/" />}
+              {!user && <Signup />}
+              {/* after signup, redirects to home */}
+              {/* if user is null, shows sign up page */}
             </Route>
           </Switch>
         </BrowserRouter>
