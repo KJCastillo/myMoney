fa185b0deae69f30fa43d7c9608175dee76efe7d
Kevin Castillo <kevincastillo318@gmail.com>
2022-10-01 20:24:54 -0700

    ordering firestore queries

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,8 @@ import { useCollection } from "../hooks/useCollection";
 export default function Home() {
   const { user } = useAuthContext();
   const { documents, error } = useCollection("transactions",
-    ["uid", "==", user.uid]
+    ["uid", "==", user.uid],
+    ["createdAt", "desc"]
   );
 
   return (
